7f3e40f432eb2759fb22945c37f0b5468e9a0fd2
Sebastian Salazar <sasalazar10@espe.edu.ec>
2024-08-03 23:18:42 -0500

    [UPD] Get ItemCollection by counterpart and company_id

--- src/main/java/com/banquito/corecobros/order/dto/ItemCollectionDTO.java
+++ src/main/java/com/banquito/corecobros/order/dto/ItemCollectionDTO.java
@@ -2,9 +2,9 @@ package com.banquito.corecobros.order.dto;
 
 import java.math.BigDecimal;
 
-
 public class ItemCollectionDTO {
     private Integer orderId;
+    private String uniqueId;
     private String debtorName;
     private String counterpart;
     private BigDecimal collectionAmount;
@@ -13,35 +13,49 @@ public class ItemCollectionDTO {
     public Integer getOrderId() {
         return orderId;
     }
+
     public void setOrderId(Integer orderId) {
         this.orderId = orderId;
     }
+
+    public String getUniqueId() {
+        return uniqueId;
+    }
+
+    public void setUniqueId(String uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+
     public String getDebtorName() {
         return debtorName;
     }
+
     public void setDebtorName(String debtorName) {
         this.debtorName = debtorName;
     }
+
     public String getCounterpart() {
         return counterpart;
     }
+
     public void setCounterpart(String counterpart) {
         this.counterpart = counterpart;
     }
+
     public BigDecimal getCollectionAmount() {
         return collectionAmount;
     }
+
     public void setCollectionAmount(BigDecimal collectionAmount) {
         this.collectionAmount = collectionAmount;
     }
+
     public String getStatus() {
         return status;
     }
+
     public void setStatus(String status) {
         this.status = status;
     }
 
-    
-    
 }
-
