069950d0b36ab334df608906dbdefaeb1433945b
Rafch17 <randyni46@gmail.com>
2024-08-20 00:29:29 -0500

    probando xd

--- src/main/java/com/banquito/corecobros/order/service/ItemCollectionService.java
+++ src/main/java/com/banquito/corecobros/order/service/ItemCollectionService.java
@@ -3,6 +3,7 @@ package com.banquito.corecobros.order.service;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,13 +11,20 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.banquito.corecobros.order.dto.AccountTransactionDTO;
 import com.banquito.corecobros.order.dto.CompanyDTO;
+import com.banquito.corecobros.order.dto.ItemAutomaticDebitDTO;
 import com.banquito.corecobros.order.dto.ItemCollectionDTO;
+import com.banquito.corecobros.order.dto.OrderDTO;
 import com.banquito.corecobros.order.dto.ResponseItemCommissionDTO;
+import com.banquito.corecobros.order.model.AutomaticDebitPaymentRecord;
+import com.banquito.corecobros.order.model.CollectionPaymentRecord;
 import com.banquito.corecobros.order.model.ItemCollection;
 import com.banquito.corecobros.order.model.Order;
 import com.banquito.corecobros.order.repository.ItemCollectionRepository;
@@ -24,6 +32,7 @@ import com.banquito.corecobros.order.repository.OrderRepository;
 import com.banquito.corecobros.order.util.mapper.ItemCollectionMapper;
 import com.banquito.corecobros.order.util.uniqueId.UniqueIdGeneration;
 
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
@@ -48,7 +57,7 @@ public class ItemCollectionService {
     }
 
     public List<ItemCollectionDTO> findByCounterpartAndCompany(String counterpart, String companyId) {
-        WebClient webClient = this.webClientBuilder.baseUrl("http://localhost:8080/api/v1/companies").build();
+        WebClient webClient = this.webClientBuilder.baseUrl("http://localhost:9090/company-microservice/api/v1/companies").build();
         CompanyDTO company = webClient.get()
                 .uri("/{uniqueId}", companyId)
                 .accept(MediaType.APPLICATION_JSON)
@@ -201,5 +210,121 @@ public class ItemCollectionService {
             }
         }
         return uniqueId;
+    }
+
+    
+    @Transactional
+    public void processPayment(Integer itemCollectionId) {
+        log.info("Iniciando procesamiento del recaudo...");
+        ItemCollectionDTO itemCollectioDto = this.obtainItemCollectionById(itemCollectionId);
+        ItemCollection itemCollection = this.itemCollectionRepository.findById(itemCollectioDto.getId())
+                .orElseThrow(() -> new RuntimeException("No se encontro la orden con el ID " + itemCollectioDto.getId()));
+        log.info("Iniciando transaccion del recaudo...");
+        WebClient webClient = WebClient.builder().baseUrl("http://localhost:8080/Account-Microservice/api/v1/account-transactions").build();
+        AccountTransactionDTO transactionDTO = AccountTransactionDTO.builder()
+                    .accountId(47)
+                    .codeChannel("CHA007363")
+                    .transactionType("DEB")
+                    .reference("Recaudo en Ventanilla ")
+                    .amount(itemCollection.getCollectionAmount())
+                    .creditorAccount("2273445678")
+                    .debitorAccount("Inventarme las cuentas")
+                    .commission(BigDecimal.valueOf(1.50))
+                    .createDate(LocalDateTime.now())
+                    .parentTransactionKey(null)
+                    .status("")
+                    .build();
+        Mono<AccountTransactionDTO> responseMono = webClient.post()
+                        .uri("")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(transactionDTO)
+                        .retrieve()
+                        .bodyToMono(AccountTransactionDTO.class);
+        
+        
+        itemCollection.setStatus("PAG");
+
+        CollectionPaymentRecord collectionPaymentRecord = new CollectionPaymentRecord();
+        collectionPaymentRecord.setItemCollectionId(itemCollection.getId());
+        collectionPaymentRecord.setCollectionAmount(itemCollection.getCollectionAmount());
+        collectionPaymentRecord.setPaymentType("TOT");
+        collectionPaymentRecord.setPaymentDate(LocalDateTime.now());
+        collectionPaymentRecord.setOutstandingBalance(BigDecimal.valueOf(0.00));
+        collectionPaymentRecord.setChannel("VEN");
+
+
+        /* 
+        List<OrderDTO> orders = this.getActiveOrdersByServiceId("JXM0025321");
+        log.info("Órdenes activas encontradas: {}", orders.size());
+        
+        for (OrderDTO order : orders) {
+            log.info("Procesando orden: {}", order.getOrderId());
+            List<ItemAutomaticDebitDTO> items = itemAutomaticDebitService.getItemsByOrderIdAndStatus(order.getOrderId(), "PEN");
+            log.info("Items pendientes encontrados para la orden {}: {}", order.getOrderId(), items.size());
+    
+            //WebClient webClient = WebClient.builder().baseUrl("http://localhost:8080/Account-Microservice/api/v1/account-transactions").build();
+            for (ItemAutomaticDebitDTO item : items) {
+                // String companyName = this.getCompanyNameByAccountId(order.getAccountId());
+                log.info("Procesando item {} de la orden {}, cuenta deudora: {}, monto: {}", item.getId(), order.getOrderId(), item.getDebitAccount(), item.getDebitAmount());
+
+                AccountTransactionDTO transactionDTO = AccountTransactionDTO.builder()
+                    .accountId(47)
+                    .codeChannel("CHA007363")
+                    .transactionType("DEB")
+                    .reference("COBRO AUTOMATICO ")
+                    .amount(item.getDebitAmount())
+                    .creditorAccount("2273445678")
+                    .debitorAccount(item.getDebitAccount())
+                    .commission(BigDecimal.valueOf(1.50))
+                    .createDate(LocalDateTime.now())
+                    .parentTransactionKey(null)
+                    .status("")
+                    .build();
+    
+                try {
+                    log.info("Enviando solicitud de débito automático para item {}...", item.getId());
+                    Mono<AccountTransactionDTO> responseMono = webClient.post()
+                        .uri("")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(transactionDTO)
+                        .retrieve()
+                        .bodyToMono(AccountTransactionDTO.class);
+    
+                    AccountTransactionDTO response = responseMono.block();
+                    log.info("Respuesta recibida para item {}: {}", item.getId(), response);
+    
+                    AccountTransactionDTO updatedResponse = AccountTransactionDTO.builder()
+                        .accountId(response.getAccountId())
+                        .codeChannel(response.getCodeChannel())
+                        .amount(response.getAmount())
+                        .debitorAccount(response.getDebitorAccount())
+                        .creditorAccount(response.getCreditorAccount())
+                        .commission(response.getCommission())
+                        .transactionType(response.getPendiente().compareTo(BigDecimal.ZERO) > 0 ? "PAR" : "TOT")
+                        .reference(response.getReference())
+                        .parentTransactionKey(response.getParentTransactionKey())
+                        .createDate(response.getCreateDate())
+                        .status(response.getPendiente().compareTo(BigDecimal.ZERO) > 0 ? "PEN" : "PAG")
+                        .pendiente(response.getPendiente())
+                        .build();
+    
+                    // Save the record using your repository or service
+                    AutomaticDebitPaymentRecord automaticDebitPaymentRecord = new AutomaticDebitPaymentRecord();
+                    automaticDebitPaymentRecord.setItemAutomaticDebitId(item.getId());
+                    automaticDebitPaymentRecord.setUniqueId(paymentRecordService.generateUniqueId());
+                    automaticDebitPaymentRecord.setOutstandingBalance(updatedResponse.getPendiente());
+                    automaticDebitPaymentRecord.setDebitAmount(updatedResponse.getAmount());
+                    automaticDebitPaymentRecord.setPaymentDate(updatedResponse.getCreateDate());
+                    automaticDebitPaymentRecord.setPaymentType(updatedResponse.getTransactionType());
+                    automaticDebitPaymentRecord.setStatus(updatedResponse.getStatus());
+    
+                    automaticDebitPaymentRecordRepository.save(automaticDebitPaymentRecord);
+                    log.info("Registro de débito automático guardado para item {}.", item.getId());
+                } catch (Exception e) {
+                    log.error("Error al procesar el débito del item {}: {}", item.getId(), e.getMessage(), e);
+                }
+            }
+        }
+        log.info("Procesamiento de débito automático completado.");*/
     }
 }
